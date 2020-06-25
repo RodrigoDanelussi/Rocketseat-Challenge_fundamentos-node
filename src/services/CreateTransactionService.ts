1c633807af3e1666638418d2a40c1f60107d1586
RodrigoDanelussi <rodrigodanelussi@gmail.com>
2020-06-25 04:47:33 -0300

    Finish

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,34 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    // TODO
+    if (type === 'outcome') {
+      if (this.transactionsRepository.getBalance().total - value < 0)
+        throw Error('Value not avaliable');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
